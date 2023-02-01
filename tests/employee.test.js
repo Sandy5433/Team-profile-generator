01c92b82fe372de05bf77e58b490fb99cc9b849d
Sandy Hung <sandyhung83@yahoo.com.tw>
2023-02-01 21:14:27 +1100

    updated test codes for employee

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -11,7 +11,43 @@ describe("Employee", () => {
             expect(obj.id).toEqual(id);
             expect(obj.email).toEqual(email);
         });
+             
+    });
 
-        
+    describe("getName", () => {
+        it("should return the name of the object", ()=>{
+            const name = "Sandy";
+            const id = 3;
+            const email = "sandy@gmail.com";
+            const obj = new Employee(name, id, email);
+            expect(obj.getName()).toEqual(name);
+        });
+    })
+
+    describe("getId", () => {
+        it("should return the id of the object", ()=>{
+            const name = "Sandy";
+            const id = 3;
+            const email = "sandy@gmail.com";
+            const obj = new Employee(name, id, email);
+            expect(obj.getId()).toEqual(id);
+        });
+    })
+
+    describe("getEmail", () => {
+        it("should return the email of the object", ()=>{
+            const name = "Sandy";
+            const id = 3;
+            const email = "sandy@gmail.com";
+            const obj = new Employee(name, id, email);
+            expect(obj.getEmail()).toEqual(email);
+        });
+    })
+
+    describe("getRole", () => {
+        it("should return the value to Employee", ()=>{
+            const obj = new Employee();
+            expect(obj.getRole()).toEqual("Employee");
+        });
     })
 })
